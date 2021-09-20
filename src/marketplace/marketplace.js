f45649eec347a90a71148b3a91b2f84af8e57f99
Tejas Kothari <tejas23682320@gmail.com>
2021-09-20 18:50:04 +0800

    Marketplace my items page

--- src/marketplace/marketplace.js
+++ src/marketplace/marketplace.js
@@ -2,12 +2,26 @@ import { useState } from "react";
 import "./marketplace.scss";
 import sampleNFTImage from "../images/ennj-artwork-1080x675.png";
 import algoIcon from "../images/algoIcon.png";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addItem } from "../storeSlice.js";
 import MyAlgoConnect from "@randlabs/myalgo-connect";
 import algosdk from "algosdk";
+import walletshs from "../images/001-wallet.svg";
+import { useHistory } from "react-router-dom";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+} from "react-router-dom";
 
 export default function Marketplace() {
+  let history = useHistory();
+  let match = useRouteMatch();
+  console.log(match);
+  const myBoughtItems = useSelector((state) => state.store.myItems);
   const dispatch = useDispatch();
 
   const sendTx = async () => {
@@ -53,30 +67,73 @@ export default function Marketplace() {
     <div className="marketplace">
       <div className="marketplace-header">
         <p>AlgoRoyale NFT Marketplace</p>
+
+        {match.path === "/marketplace" && match.isExact ? (
+          <div onClick={() => history.push("/marketplace/myitems")}>
+            <img src={walletshs} />
+            <p>My items</p>
+          </div>
+        ) : (
+          <div onClick={() => history.push("/marketplace")}>
+            <p>Marketplace</p>
+          </div>
+        )}
       </div>
       <div style={{ padding: "0 200px" }}>
-        <h1>Featured game items</h1>
-        <div className="itemsContainer">
-          {itemArr.map((item, index) => (
-            <div className="itemContainer">
-              <img src={sampleNFTImage} />
-              <div className="itemInfo">
-                <h4>{item.itemName}</h4>
-                <div className="line"></div>
-                <div className="priceInfo">
-                  <p>Price</p>
-                  <br />
-                  <div style={{ display: "flex", alignItems: "center" }}>
-                    <img src={algoIcon} width="25px" />
-                    <p>{item.price}</p>
-                    <h6>(US$ {item.price * 1.81})</h6>
+        <Switch>
+          <Route path={match.path} exact>
+            <h1>Featured game items</h1>
+            <div className="itemsContainer">
+              {itemArr.map((item, index) => (
+                <div className="itemContainer">
+                  <img src={sampleNFTImage} />
+                  <div className="itemInfo">
+                    <h4>{item.itemName}</h4>
+                    <div className="line"></div>
+                    <div className="priceInfo">
+                      <p>Price</p>
+                      <br />
+                      <div style={{ display: "flex", alignItems: "center" }}>
+                        <img src={algoIcon} width="25px" />
+                        <p>{item.price}</p>
+                        <h6>(US$ {item.price * 1.81})</h6>
+                      </div>
+                    </div>
+                    <button onClick={() => buyItem(item, index)}>
+                      Buy NFT
+                    </button>
                   </div>
                 </div>
-                <button onClick={() => buyItem(item, index)}>Buy NFT</button>
-              </div>
+              ))}
             </div>
-          ))}
-        </div>
+          </Route>
+          <Route path={`${match.path}/myitems`}>
+            <h1>My items</h1>
+            <div className="itemsContainer">
+              {myBoughtItems.map((item, index) => (
+                <div className="itemContainer">
+                  <img src={sampleNFTImage} />
+                  <div className="itemInfo">
+                    <h4>{item.itemName}</h4>
+                    <div className="line"></div>
+                    <div className="priceInfo">
+                      <p>Bought at</p>
+                      <br />
+                      <div style={{ display: "flex", alignItems: "center" }}>
+                        <img src={algoIcon} width="25px" />
+                        <p>{item.price}</p>
+                        <h6>(US$ {item.price * 1.81})</h6>
+                      </div>
+                    </div>
+                    <button onClick={() => buyItem(item, index)}>
+                      Sell NFT
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </Route>
+        </Switch>
       </div>
     </div>
   );
